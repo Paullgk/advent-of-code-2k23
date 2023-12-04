75c15de364565c65b6982a8c09b90fad1fad23a1
Paul Le Guen de Kerneizon <paul-le-guen@outlook.fr>
2023-12-05 00:36:31 +0100

    day2: add part 2
    Add stable solution for part 2.

--- day2/compute_possible_game.py
+++ day2/compute_possible_game.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-def check_bag(bag, input_line):
+# Solutions for both parts of day 2 of AOC 2023. Make sure you put your data in data folder.
 
+def check_bag(bag, input_line):
+    # Return which game is compatible with the given bag 
+    
     if input_line == '':
         return 0
     cubes = [0,0,0] # RGB format
@@ -34,6 +37,41 @@ def check_bag(bag, input_line):
         cubes = [0,0,0]
     return game_id
 
+def get_min_bag(input_line):
+    # Return minimum cubes needed to make a game with your bag
+
+    if input_line == '':
+        return 0
+    
+    cubes = [0,0,0] # RGB format
+    input_line = input_line.replace(' ', '')
+    game_id = input_line.split(':')[0]
+    game_id = int(game_id.replace("Game",""))
+    game_content = input_line.split(':')[1]
+    game_content = game_content.split(';')
+
+    for set in game_content:
+        # Retrieve each number of cubes of the set
+        for color in set.split(','):
+            if color.find('red') != -1:
+                red_cubes = color.split('red')[0]
+
+                if int(red_cubes) > cubes[0]:
+                    cubes[0] = int(red_cubes)
+
+            elif color.find('green') != -1:
+                green_cubes = color.split('green')[0]
+
+                if int(green_cubes) > cubes[1]:
+                    cubes[1] = int(green_cubes)
+
+            elif color.find('blue') != -1:
+                blue_cubes = color.split('blue')[0]
+
+                if int(blue_cubes) > cubes[2]:
+                    cubes[2] = int(blue_cubes)
+
+    return cubes[0]*cubes[1]*cubes[2]
 
 bag = [12,13,14] # RGB format
 
@@ -45,4 +83,10 @@ sum = 0
 for line in extracted_lines:
     game_id = check_bag(bag, line)
     sum = sum + game_id
-print(f"Total sum is {sum}")
+print(f"Total sum for part one is {sum}")
+
+sum = 0
+for line in extracted_lines:
+    power = get_min_bag(line)
+    sum = sum + power
+print(f"Total sum for part one is {sum}")
